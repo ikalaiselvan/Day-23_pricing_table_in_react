b08420bbff424311c1960b5348b3cdb92a0fe42c
Kalaiselvan <ikalaiselvan@users.noreply.github.com>
2022-12-21 21:58:18 +0530

    component implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,86 @@
 import "./App.css";
 import Pricing from './components/Pricing';
+
 function App() {
+  let data = [
+    {
+      plan: "FREE",
+      price: "0",
+      users: "Single User",
+      usersEnabler: true,
+      fontWeight: false,
+      storage: "5GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: false,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: false,
+      subDomain: "Free Subdomain",
+      subDomainEnabler: false,
+      subDomainUnlimit: false,
+      statusReports: "Monthly Status Reports",
+      statusReportsEnabler: false,
+    },
+    {
+      plan: "Plus",
+      price: "9",
+      users: "5 User",
+      usersEnabler: true,
+      fontWeight: true,
+      storage: "50GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Free Subdomain",
+      subDomainEnabler: true,
+      subDomainUnlimit: false,
+      statusReports: "Monthly Status Reports",
+      statusReportsEnabler: false,
+    },
+    {
+      plan: "PRO",
+      price: "49",
+      users: "Unlimited Users",
+      usersEnabler: true,
+      fontWeight: true,
+      storage: "150GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Free Subdomains",
+      subDomainEnabler: true,
+      subDomainUnlimit: true,
+      statusReports: "Monthly Status Reports",
+      statusReportsEnabler: true,
+    },
+  ];
+
   return (
-    <div className="App">
-      <Pricing/>
-    </div>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+          <Pricing data={data[0]}/>
+          <Pricing data={data[1]}/>
+          <Pricing data={data[2]}/>
+          </div>
+        </div>
+      </section>
   );
 }
 
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,90 +1,123 @@
 import React from "react";
 
-function Pricing() {
-  return <div>
-    <section className="pricing py-5">
-    <div className="container">
-    <div className="row">
-      {/* <!-- Free Tier --> */}
-      <div className="col-lg-4">
-        <div className="card mb-5 mb-lg-0">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
-            <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
-                Private Projects</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
-                Phone Support</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain
-              </li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div className="d-grid">
-              <a href="javascript(void)" className="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- Plus Tier --> */}
-      <div className="col-lg-4">
-        <div className="card mb-5 mb-lg-0">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
-            <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div className="d-grid">
-              <a href="javascript(void)" className="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- Pro Tier --> */}
-      <div className="col-lg-4">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
-            <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong>
-              </li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free
-                Subdomains</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
-            </ul>
-            <div className="d-grid">
-              <a href="javascript(void)" className="btn btn-primary text-uppercase">Button</a>
-            </div>
-          </div>
+function Pricing(props) {
+  return(
+  <div className="col-lg-4">
+    <div className="card mb-5 mb-lg-0">
+      <div className="card-body">
+        <h5 className="card-title text-muted text-uppercase text-center">
+          {props.data.plan}
+        </h5>
+        <h6 className="card-price text-center">
+          ${props.data.price}
+          <span className="period">/month</span>
+        </h6>
+        <hr />
+        <ul className="fa-ul">
+          <li className={props.data.usersEnabler ? "" : "text-muted"}>
+            <span className="fa-li ">
+              <i
+                className={
+                  props.data.usersEnabler ? "fas fa-check " : "fas fa-times"
+                }
+              ></i>
+            </span>
+            <span className={props.data.fontWeight ? "fontThick" : ""}>
+              {props.data.users}
+            </span>
+          </li>
+          <li className={props.data.storageEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.storageEnabler ? "fas fa-check" : "fas fa-times"
+                }
+              ></i>
+            </span>
+            {props.data.storage}
+          </li>
+          <li className={props.data.publicProjectsEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.publicProjectsEnabler
+                    ? "fas fa-check"
+                    : "fas fa-times"
+                }
+              ></i>
+            </span>
+            {props.data.publicProjects}
+          </li>
+          <li className={props.data.communityAccessEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.communityAccessEnabler
+                    ? "fas fa-check"
+                    : "fas fa-times"
+                }
+              ></i>
+            </span>
+            {props.data.communityAccess}
+          </li>
+          <li className={props.data.privateProjectsEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.privateProjectsEnabler
+                    ? "fas fa-check"
+                    : "fas fa-times"
+                }
+              ></i>
+            </span>
+            {props.data.privateProjects}
+          </li>
+          <li className={props.data.phoneSupportEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.phoneSupportEnabler
+                    ? "fas fa-check"
+                    : "fas fa-times"
+                }
+              ></i>
+            </span>
+            {props.data.phoneSupport}
+          </li>
+          <li className={props.data.subDomainEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.subDomainEnabler ? "fas fa-check" : "fas fa-times"
+                }
+              ></i>
+            </span>
+            <span className="fontThick">
+              {props.data.subDomainUnlimit ? "Unlimited" : ""}
+            </span>{" "}
+            {props.data.subDomain}
+          </li>
+          <li className={props.data.statusReportsEnabler ? "" : "text-muted"}>
+            <span className="fa-li">
+              <i
+                className={
+                  props.data.statusReportsEnabler
+                    ? "fas fa-check"
+                    : "fas fa-times"
+                }
+              ></i>
+            </span>
+            {props.data.statusReports}
+          </li>
+        </ul>
+        <div className="d-grid">
+          <a href="javascript(void)" className="btn btn-primary text-uppercase">
+            Button
+          </a>
         </div>
       </div>
     </div>
   </div>
-</section>
-  </div>;
-}
+)}
 
 export default Pricing;
